1c6e88d9a8528188d150ac4f6ec82847a1a0c13a
Girish goud <girishgoud@Girishs-MacBook-Pro.local>
2025-06-05 18:56:28 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,19 +1,10 @@
 import requests
 
-API_KEY = "b0bc040f3eda49b7a97220712250506"  # ← replace with your real key
+API_KEY = "b0bc040f3eda49b7a97220712250506"  # replace with your key
 city = "delray beach"
-
 url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
-
-# Make the API request
 response = requests.get(url)
-
-# Convert JSON response to Python dict
 data = response.json()
-
-# Extract useful info
 temp = data["current"]["temp_c"]
 condition = data["current"]["condition"]["text"]
-
-# Print result
 print(f"🌤️ Weather in {city}: {temp}°C, {condition}")
